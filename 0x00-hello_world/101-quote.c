cc7eb4d1b69c3eb6c48955e519ae8673ea321c11
maryammary99 <meryemouali16@gmail.com>
2023-07-13 14:35:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t message_len = sizeof(message) - 1;
 
-	write(2, message, sizeof(message) - 1);
+	write(2, message, message_len);
 
 	return (1);
 }
