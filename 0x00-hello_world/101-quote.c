56d3c10bdf0bfcd3f1334204f8285526d3e1a0de
maryammary99 <meryemouali16@gmail.com>
2023-07-13 14:32:16 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,7 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/syscall.h>
 
-#define STDERR_FILENO 2
-
-int main() {
-    const char message[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+int main(void) {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
     return 1;
 }
-
