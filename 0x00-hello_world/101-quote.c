1fd7fc0301d54ed2112c1d05d1fa3abdb937ffaf
maryammary99 <meryemouali16@gmail.com>
2023-07-13 14:28:05 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+#define STDERR_FILENO 2
+
+void print(const char *str) {
+    while (*str != '\0') {
+        syscall(SYS_write, STDERR_FILENO, str, 1);
+        str++;
+    }
 }
+
+int main() {
+    const char *message = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    print(message);
+    return 1;
+}
+
